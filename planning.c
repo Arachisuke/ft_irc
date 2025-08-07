f945d2c842084a574c655081090d21ec983533c7
Mac Os <macos@Macintosh.local>
2025-08-07 17:19:06 +0200

    planning

--- planning.c
+++ planning.c
@@ -7,3 +7,11 @@
 // plusieurs client.
 // commande bloquante
 // signaux, commande partiel.
+
+
+
+// ANDY
+// vu que la connexion est etabli, entre le serveur et le client
+// je lis les entree avec la fonction lecture qui renvoie entry, une std::string
+// avec entry -> comparer avec les cmd qu'on doit implementer et ensuite les implementer.
+// dans les cmd a implementer c'est ecris dnas le sujet, et j'ai mis bcp bcp d'info sur les cmd dans descriptif si t'arrives a lire, et faut surtout ce referer aux lien 2, de thib, et au premier de felipe.
