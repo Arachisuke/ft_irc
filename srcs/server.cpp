59d269d331ce92cc38d17416953f27e145308ec6
Mac Os <macos@Macintosh.local>
2025-08-11 20:01:20 +0200

    myfinalremoveandychange

--- srcs/server.cpp
+++ srcs/server.cpp
@@ -3,16 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   server.cpp                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ankammer <ankammer@student.42.fr>          +#+  +:+       +#+        */
+/*   By: macos <macos@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/10 12:42:01 by macos             #+#    #+#             */
-/*   Updated: 2025/08/11 15:12:47 by ankammer         ###   ########.fr       */
+/*   Updated: 2025/08/11 19:57:56 by macos            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../header/server.hpp"
 
-Server::Server(int port, std::string password) : _port(port), _password(password)
+Server::Server()
 {}
 Server::~Server() {
     // Close all client sockets a faire
@@ -21,8 +21,15 @@ Server::~Server() {
     std::cout << "Server shutdown" << std::endl;
 }
 
+void clearsinzero(struct sockaddr_in *addr)
+{
+    for (int i = 0; i < 8; ++i)
+        addr->sin_zero[i] = 0;
+}
+
 int Server::Init(int epfd)
 {
+    int yes = 1;
     this->hote.sin_family = AF_INET;
     this->hote.sin_port = htons(6667);
     this->hote.sin_addr.s_addr = htonl(INADDR_ANY);
@@ -30,14 +37,18 @@ int Server::Init(int epfd)
     this->fd = socket(AF_INET, SOCK_STREAM, 0);
     if (fd == -1)
         return(std::cout << "ERR_FD" << std::endl, 50);
+    if (setsockopt(this->fd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(yes)) == -1) 
+        return(close(this->fd), 50);
+
+    if (bind(this->fd, reinterpret_cast<struct sockaddr*>(&this->hote), sizeof(this->hote)) == -1)
+        return(close(this->fd),  std::cout << "ERR_BIND" << std::endl, 50);
+
+    if(listen(this->fd, 10) == -1)
+        return(close(this->fd), std::cout << "ERR_LISTEN" << std::endl, 50);
+    
     fcntl(fd, F_SETFL, O_NONBLOCK); // Rendre non bloquant
     this->epfd = epfd;
-    this->events.events = EPOLLIN;
-    epoll_ctl(this->epfd, EPOLL_CTL_ADD, this->fd, &this->events);
-    return (0); // ajout du return en cas de succes
-}
-void Server::clearsinzero(struct sockaddr_in *addr) // ajout de la fonction sinzero initialement dans le main
-{
-    for (int i = 0; i < 8; ++i)
-        addr->sin_zero[i] = 0;
+    this->events[0].events = EPOLLIN;
+    epoll_ctl(this->epfd, EPOLL_CTL_ADD, this->fd, &this->events[0]);
+    return(0);
 }
