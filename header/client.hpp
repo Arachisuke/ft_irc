7ae8e2ed317e3832a06eb0c8750e0e9e9bbd71dc
Mac Os <macos@Macintosh.local>
2025-08-11 08:13:01 +0200

    testepoll

--- header/client.hpp
+++ header/client.hpp
@@ -3,10 +3,67 @@
 /*                                                        :::      ::::::::   */
 /*   client.hpp                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ankammer <ankammer@student.42.fr>          +#+  +:+       +#+        */
+/*   By: macos <macos@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/08/07 14:29:41 by ankammer          #+#    #+#             */
-/*   Updated: 2025/08/07 14:29:42 by ankammer         ###   ########.fr       */
+/*   Created: 2025/08/07 14:29:51 by ankammer          #+#    #+#             */
+/*   Updated: 2025/08/11 08:00:58 by macos            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+#include <unistd.h>
+#include <string>
+#include <cstring>
+#include <fcntl.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <fcntl.h>
+#include <iostream>
+#include <unistd.h>
+#include <string>
+#include <iostream>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <string>
+#include <cstring>
+#include <fcntl.h>
+#include <sys/epoll.h>
+#include <vector>
+#include <cstdlib>
+
+class Client
+{
+
+    public:
+        Client(int hote);
+        ~Client();
+        void ReadMsg();
+        void PushMsg(std::string msg);
+        int Init(int hote);
+        int fd;
+        int RPL_WELCOME;
+        int RPL_INFO;
+        int RPL;
+
+
+    std::string nickname; // 9 length
+    std::string username; // 9 length
+    std::string realname;  // 128 length
+    std::string hostname; // 63 length
+    std::string servername; // 63 length
+    std::string mode; // 3 length 
+    private:
+        std::string translationclient_to_server(std::string message);
+        int epfd;
+        int hote;
+        struct epoll_event events;
+        struct sockaddr_in client;
+        socklen_t size_of_client;
+        std::vector<int> client_list;
+        std::string password;
+        std::string entry;
+        int bytes;
+         
+        
+};
---
+++ srcs/server.cpp
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   server.cpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: macos <macos@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/10 12:42:01 by macos             #+#    #+#             */
+/*   Updated: 2025/08/10 16:45:16 by macos            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "server.hpp"
+
+Server::Server(int port, std::string password) : _port(port), _password(password)
+{}
+Server::~Server() {
+    // Close all client sockets a faire
+    // Close the server socket
+    close(this->fd);
+    std::cout << "Server shutdown" << std::endl;
+}
+
+int Server::Init(int epfd)
+{
+    this->hote.sin_family = AF_INET;
+    this->hote.sin_port = htons(6667);
+    this->hote.sin_addr.s_addr = htonl(INADDR_ANY);
+    clearsinzero(&hote);
+    this->fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (fd == -1)
+        return(std::cout << "ERR_FD" << std::endl, 50);
+    fcntl(fd, F_SETFL, O_NONBLOCK); // Rendre non bloquant
+    this->epfd = epfd;
+    this->events.events = EPOLLIN;
+    epoll_ctl(this->epfd, EPOLL_CTL_ADD, this->fd, &this->events);
+}
