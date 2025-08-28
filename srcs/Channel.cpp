149f0fcde30158e7c78d55a9175b0db13ffc64b1
Andy Kammerer <ankammer@f6r7s4.clusters.42paris.fr>
2025-08-28 12:37:41 +0200

    add: ajout cmd invite fini

--- srcs/Channel.cpp
+++ srcs/Channel.cpp
@@ -6,7 +6,7 @@
 /*   By: ankammer <ankammer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/25 16:53:12 by ankammer          #+#    #+#             */
-/*   Updated: 2025/08/25 18:35:06 by ankammer         ###   ########.fr       */
+/*   Updated: 2025/08/28 12:31:28 by ankammer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -67,4 +67,11 @@ void Channel::removeOperator(Client *operatorToRemove)
 bool Channel::getOnInviteOnly() const
 {
     return (_onInviteOnly);
+}
+
+bool Channel::channelIsFull()
+{
+    if (_maxUsers < 0)
+        return(0);
+    return(_users.size() >= static_cast<unsigned long>(_maxUsers));
 }
